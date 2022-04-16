604acc059f65f5ee638f11849f8b3236acdfbe28
richard-allcca <53624835+richard-allcca@users.noreply.github.com>
2022-04-15 22:14:49 -0500

    UPDATE port

--- models/server.models.js
+++ models/server.models.js
@@ -9,7 +9,7 @@ class Server {
 
   constructor() {
     this.app = express();
-    this.port = process.env.PORT;
+    this.port = process.env.PORT || 8085;
 
     // rutas end point
     this.paths = {
